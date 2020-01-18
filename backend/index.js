ddd22bb8344d20986ce7dde90ee17add55f2cf44
Diego Santos <diegosantos3208@gmail.com>
2020-01-18 14:43:28 -0300

    commit on master

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    
+    return response.json({
+        nome: "Diego",
+        sobrenome: "Santos",
+        idade: 15
+    });
 });
 
 
